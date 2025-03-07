26aa0ae29153651bfa34e203159ac9670892b3dc
Mart Mbithi <martdevelopers254@gmail.com>
2025-03-07 12:58:48 +0300

    Craft Bids - Add Checker To Prevent Multiple Biddings

--- app/helpers/bids.php
+++ app/helpers/bids.php
@@ -81,10 +81,10 @@ if (isset($_POST['Place_Bid'])) {
         $err = "Your Bid Is Lower Than The Artwork Price";
     } else {
         /* Prevent Bidding Multiple Times On Artwork Before 4 hours Elapses */
-        $sql = "SELECT * FROM bids WHERE bid_user_id  = '{$bid_user_id}' AND bid_product_id = '{$bid_product_id}' AND bid_date >= DATE_SUB(NOW(), INTERVAL 4 HOUR)";
+        $sql = "SELECT * FROM bids WHERE bid_user_id  = '{$bid_user_id}' AND bid_product_id = '{$bid_product_id}' AND bdi_status = 'Pending'";
         $result = mysqli_query($mysqli, $sql);
         if (mysqli_num_rows($result) > 0) {
-            $err = "You Can Only Bid Once Every 4 Hours";
+            $err = "You Can Only Bidm Again Once Your Current Bid Is Processed";
         } else {
             if (mysqli_query(
                 $mysqli,
@@ -98,6 +98,7 @@ if (isset($_POST['Place_Bid'])) {
         }
     }
 }
+
 /* Cancel Bid */
 
 /* Process Bid */
