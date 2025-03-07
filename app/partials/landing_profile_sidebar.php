28ec4e8aed792f89d2b5d5f07c395a0cfcf26c1a
Mart Mbithi <martdevelopers254@gmail.com>
2025-03-07 11:33:19 +0300

    Added Landing My Bids

--- app/partials/landing_profile_sidebar.php
+++ app/partials/landing_profile_sidebar.php
@@ -7,9 +7,9 @@
                 <div class="ec-vendor-block-items">
                     <ul>
                         <li><a href="landing_profile">User Profile</a></li>
+                        <li><a href="landing_my_bids">My Bids</a></li>
                         <li><a href="landing_purchase_history">History</a></li>
                         <li><a href="landing_wishlist">Wishlist</a></li>
-                        <li><a href="landing_cart">Cart</a></li>
                         <li><a href="landing_track_order">Track Order</a></li>
                         <li><a href="landing_auth">Authentication Settings</a></li>
                         <li><a href="landing_seller_dashboard">Seller Dashboard</a></li>
